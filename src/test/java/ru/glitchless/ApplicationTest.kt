5032c188ea78310403afaede3b2d28414a4e0bf6
lionzxy <nikita@kulikof.ru>
2017-10-18 15:33:08 +0300

    Switch to Kotlin

---
+++ src/test/java/ru/glitchless/ApplicationTest.kt
@@ -0,0 +1,13 @@
+package ru.glitchless
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+class ApplicationTest {
+    @Test
+    fun contextLoads() {}
+}
