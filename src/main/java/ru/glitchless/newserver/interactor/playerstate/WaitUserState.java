0cb7710b9867be4f904a217901766586e7be4a53
lionzxy <nikita@kulikof.ru>
2017-12-13 18:44:24 +0300

    [WIP] Finish refactoring

--- src/main/java/ru/glitchless/newserver/interactor/playerstate/WaitUserState.java
+++ src/main/java/ru/glitchless/newserver/interactor/playerstate/WaitUserState.java
@@ -7,14 +7,22 @@ import ru.glitchless.game.data.packages.toclient.GameInitState;
 import ru.glitchless.newserver.data.model.ClientState;
 import ru.glitchless.newserver.data.model.WebSocketMessage;
 import ru.glitchless.newserver.data.model.WebSocketUser;
+import ru.glitchless.newserver.repository.game.GameRepository;
 import ru.glitchless.newserver.repository.lobby.PlayerRepository;
+import ru.glitchless.newserver.utils.SendMessageService;
 
 @Component
 public class WaitUserState implements IPlayerState {
     private final PlayerRepository playerRepository;
+    private final GameRepository gameRepository;
+    private final SendMessageService sendMessageService;
 
-    public WaitUserState(PlayerRepository playerRepository) {
+    public WaitUserState(PlayerRepository playerRepository,
+                         GameRepository gameRepository,
+                         SendMessageService sendMessageService) {
         this.playerRepository = playerRepository;
+        this.gameRepository = gameRepository;
+        this.sendMessageService = sendMessageService;
     }
 
     @Override
@@ -29,8 +37,10 @@ public class WaitUserState implements IPlayerState {
             return;
         }
 
-        playerRepository.putPlayerState(forUser, new PreparingResourceState(playerRepository, secondUser));
-        playerRepository.putPlayerState(secondUser, new PreparingResourceState(playerRepository, forUser));
+        playerRepository.putPlayerState(forUser,
+                new PreparingResourceState(playerRepository, secondUser, gameRepository, sendMessageService));
+        playerRepository.putPlayerState(secondUser,
+                new PreparingResourceState(playerRepository, forUser, gameRepository, sendMessageService));
     }
 
     @Override
